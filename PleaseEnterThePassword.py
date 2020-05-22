5a250d4b3bf025deec513eb589163c458949cd2c
Abhinav S <65655891+abhinavsath@users.noreply.github.com>
2020-05-22 18:06:31 +0530

    Create PleaseEnterThePassword.py

---
+++ PleaseEnterThePassword.py
@@ -0,0 +1,30 @@
+password=input('Enter your username')
+    if password == abhi0607:
+        print('Your password is correct. Please wait a few seconds while our page is loading.')
+    elif print('Your password is incorrect. Retry again'):
+password=input('Enter your username')
+    if password == abhi0607:
+        print('Your password is correct. Please wait a few seconds while our page is loading.')
+    elif print('Your password is incorrect. Retry again'):
+password=input('Enter your username')
+    if password == abhi0607:
+        print('Your password is correct. Please wait a few seconds while our page is loading.')
+    elif  print('Your account has been locked. To unlock it, answer your secret question.'):
+question=input('What is your mothers name?')
+    if question == Meenakshi PV:
+        print('Your secret questions answer is correct. You may reset the password.')
+newpassword=input('Enter a new password.')
+input()   
+            elif print('Your secret question is incorrect. Retry again'):
+question=input('What is your mothers name?'):
+    if question == Meenakshi PV:
+        print('Your secret questions answer is correct. You may reset the password.')
+newpassword=input('Enter a new password.')
+input() 
+            elif print('Your secret question is incorrect. Retry again'):
+question=input('What is your mothers name?'):
+    if question == Meenakshi PV:
+        print('Your secret questions answer is correct. You may reset the password.')
+newpassword=input('Enter a new password.')
+input() 
+            elif print('Your session has expired.'):
