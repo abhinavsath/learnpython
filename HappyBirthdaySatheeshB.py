2044806e5c5a547693e140deed21dd1720503d70
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2020-08-11 14:21:26 +0530

    Update HappyBirthdaySatheeshB.py
    Changes made

--- HappyBirthdaySatheeshB.py
+++ HappyBirthdaySatheeshB.py
@@ -25,6 +25,6 @@ if question=='B COM':
     print ('There are 3 basic rules of B COM, that is:')
     print ('1. Debit the Reciever, Credit the Giver')
     print ('2. Debit what Comes in, Credit what Goes Out.')
-    print ('3. Debit all credits and incomes, credit all expenses and losses.')
+    print ('3. Debit all expenses and losses, credit all credits and incomes.')
     print ('Understand? It is as simple as that. Apply these 3 rules, and you will get your requirements solved.')
     print ('Thank You!')
