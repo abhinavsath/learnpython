fd0d0cdd178a71c7cd17f98f35a50a138241f32f
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-21 10:35:32 +0530

    Delete Simple Interest.py

--- Simple Interest.py
+++
@@ -1,6 +0,0 @@
-#Program to find Simple Interest
-p=int(input('Enter the Principal amount'))
-r=int(input('Enter the Rate of Interest'))
-t=int(input('Enter the Time'))
-SI=(p*r*t)/100
-print ('Simple Interest=', SI)
