78e6c0c35b56a0b3c293e54bf685abe2c6233ef5
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-21 10:35:55 +0530

    Delete When to Retire.py

--- When to Retire.py
+++
@@ -1,6 +0,0 @@
-name=input('Enter your name :')
-age=int (input('Enter your age :'))
-profession=input('Enter your profession :')
-retiring=int (input('Enter your retiring age :'))
-yearsleft=retiring-age
-print('Hello {0}, your age now is {1}. You will have to retire from{2}, {3} years from now.'.format(name,age,profession,yearsleft))
