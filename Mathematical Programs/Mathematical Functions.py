b9cf5b20a048a94c64ee7e5e72b7403c8d7d8f16
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-27 15:54:47 +0530

    Mathematical Functions
    Created the Mathematical Program to learn about the Maths Module in Python

---
+++ Mathematical Programs/Mathematical Functions.py
@@ -0,0 +1,11 @@
+# Putting ** and a number after that means exponents. For eg:- 2**2=4, 2**7=128 etc.
+#Putting // gives only integer of the division. For eg:- 7//2=3, 4//3=1 etc.
+#Putting % gives the remainder of the division. For eg:- 7%2=1, 4%3=1 etc.
+768+456
+45*7
+287/90
+5**2
+9**3
+9**2
+45//2
+45%2
