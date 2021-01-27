7ab1d0358144a0de77cd8008fd97f2d725a26f9c
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-27 15:59:52 +0530

    Python Functions
    Learnt about the Function command in Python

---
+++ Python Basics/Python Functions.py
@@ -0,0 +1,7 @@
+name=input('May I know your name?')
+def namegreet(name):
+    print ('Hello, ',name)
+job=input('May I know your hobby?')
+def jobg(job):
+    print ('Your hobby is ', job)
+print ('Hello,', name, '. Your hobby is', job,'.')
