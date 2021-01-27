4c13082c9aa06407ee4ab02ebcc9dc300c6b27ef
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-27 15:57:04 +0530

    Simple Interest
    Created the Mathematical Program to find out Simple Interest

---
+++ Mathematical Programs/Simple Interest.py
@@ -0,0 +1,6 @@
+#Program to find Simple Interest
+p=int(input('Enter the Principal amount'))
+r=int(input('Enter the Rate of Interest'))
+t=int(input('Enter the Time'))
+SI=(p*r*t)/100
+print ('Simple Interest=', SI)
