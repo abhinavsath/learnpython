1255af5bfaf9090ed17543e9b688f7f291ce9a0c
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-27 15:59:07 +0530

    Python Strings
    Learnt about the data type 'Strings'

---
+++ Python Basics/Python Data Types/Python Strings.py
@@ -0,0 +1,4 @@
+stringn=input('Enter your name')
+stringp=input('Enter the place you were born')
+stringd=input('Enter the day you were born')
+print ('Hello,', stringn + '. You were born in', stringp + ' on', stringd)
