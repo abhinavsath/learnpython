673e7b11efdaee21c063421173f86b771b38c55b
abhinavsath <65655891+abhinavsath@users.noreply.github.com>
2021-01-27 15:58:44 +0530

    Python Bool
    Learnt about the data type 'Bool'

---
+++ Python Basics/Python Data Types/Python Bool.py
@@ -0,0 +1,6 @@
+#Boolean is a Function in Python which is a set of Numbers that does not contain Zeros and None. eg:-
+print (bool(0)) #Python will return False
+print (bool(1)) #Python will return True
+print (bool(1.5)) #Pyton will return True
+print (bool(None)) #Python will return False
+print (bool('')) #Python will return False
